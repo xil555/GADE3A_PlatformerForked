eff902047998382c76564d4c2326929f10e08c42
SechabaVDM <162897708+SechabaVDM@users.noreply.github.com>
2026-04-28 15:13:03 +0200

    Nav Mesh and a bit of the AI patrol

--- Assets/AI Enemies/EnemyAI.cs
+++ Assets/AI Enemies/EnemyAI.cs
@@ -1,13 +1,19 @@
-using UnityEngine;
+ using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
     public Animator animator;
     public Transform player;
+    NavMeshAgent agent;
 
     public float detectionRange = 5f;
     public float attackRange = 2f;
 
+    public float patrolRadius = 10f;
+    public float patrolDelay = 3f;
+
+    private float patrolTimer;
     void Start()
     {
         // Get Animator safely
@@ -28,6 +34,8 @@ public class EnemyAI : MonoBehaviour
         {
             Debug.LogError("Player not found! Make sure Player tag is set.");
         }
+
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
@@ -56,17 +64,45 @@ public class EnemyAI : MonoBehaviour
     {
         animator.SetBool("isPatrolling", false);
         animator.SetBool("isAttacking", false);
+
+        agent.isStopped = false;
     }
 
     void Patrol()
     {
         animator.SetBool("isPatrolling", true);
         animator.SetBool("isAttacking", false);
-    }
 
+        patrolTimer += Time.deltaTime;
+
+        if (patrolTimer >= patrolDelay)
+        {
+            Vector3 newPos = RandomNavMeshPosition(transform.position, patrolRadius);
+            agent.SetDestination(newPos);
+
+            patrolTimer = 0f;
+        }
+        Vector3 RandomNavMeshPosition(Vector3 origin, float radius)
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * radius;
+            randomDirection += origin;
+
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomDirection, out hit, radius, NavMesh.AllAreas))
+            {
+                return hit.position;
+            }
+
+            return origin;
+        }
+        Debug.Log("Patrolling...");
+    }
+   
     void Attack()
     {
         animator.SetBool("isPatrolling", false);
         animator.SetBool("isAttacking", true);
+
+        agent.isStopped = true;
     }
 }
